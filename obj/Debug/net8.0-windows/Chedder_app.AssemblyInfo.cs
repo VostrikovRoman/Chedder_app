012c47e018c77b8a82f9949e91e7c8bddfb8d363
FroStyRo <62169048+VostrikovRoman@users.noreply.github.com>
2024-04-09 13:33:04 +0500

    Косячный коммит

--- obj/Debug/net8.0-windows/Chedder_app.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Chedder_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chedder_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac0f779bc1b07ef5598acf7771f157219fb0bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chedder_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chedder_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
