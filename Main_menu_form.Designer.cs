cd9da4fa2b18da5cb0600853899243da589381be
FroStyRo <62169048+VostrikovRoman@users.noreply.github.com>
2024-05-11 03:34:55 +0500

    2

---
+++ Main_menu_form.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Chedder_app
+{
+    partial class Main_menu_form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_menu_form));
+            panel1 = new Panel();
+            label1 = new Label();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            panel3 = new Panel();
+            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            panel4 = new Panel();
+            pictureBox3 = new PictureBox();
+            label4 = new Label();
+            panel5 = new Panel();
+            pictureBox4 = new PictureBox();
+            label5 = new Label();
+            panel6 = new Panel();
+            pictureBox5 = new PictureBox();
+            label6 = new Label();
+            panel7 = new Panel();
+            pictureBox6 = new PictureBox();
+            label7 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(203, 129, 17);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-4, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1925, 178);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Lucida Sans", 50F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(238, 208, 172);
+            label1.Location = new Point(689, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(624, 95);
+            label1.TabIndex = 0;
+            label1.Text = "Главное меню";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(145, 260);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(929, 105);
+            panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.cashier;
+            pictureBox1.Location = new Point(29, 14);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(90, 90);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Cursor = Cursors.Hand;
+            label2.Font = new Font("Lucida Sans", 35F, FontStyle.Bold);
+            label2.ForeColor = Color.FromArgb(94, 21, 4);
+            label2.Location = new Point(180, 24);
+            label2.Name = "label2";
+            label2.Size = new Size(199, 67);
+            label2.TabIndex = 1;
+            label2.Text = "Касса";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(pictureBox2);
+            panel3.Controls.Add(label3);
+            panel3.Location = new Point(145, 380);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(929, 105);
+            panel3.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.puzzle;
+            pictureBox2.Location = new Point(29, 9);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(90, 90);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Cursor = Cursors.Hand;
+            label3.Font = new Font("Lucida Sans", 35F, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(94, 21, 4);
+            label3.Location = new Point(180, 24);
+            label3.Name = "label3";
+            label3.Size = new Size(241, 67);
+            label3.TabIndex = 1;
+            label3.Text = "Сборка";
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(pictureBox3);
+            panel4.Controls.Add(label4);
+            panel4.Location = new Point(145, 500);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(929, 105);
+            panel4.TabIndex = 3;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.kitchen;
+            pictureBox3.Location = new Point(29, 9);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(90, 90);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Cursor = Cursors.Hand;
+            label4.Font = new Font("Lucida Sans", 35F, FontStyle.Bold);
+            label4.ForeColor = Color.FromArgb(94, 21, 4);
+            label4.Location = new Point(180, 24);
+            label4.Name = "label4";
+            label4.Size = new Size(198, 67);
+            label4.TabIndex = 1;
+            label4.Text = "Кухня";
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(pictureBox4);
+            panel5.Controls.Add(label5);
+            panel5.Location = new Point(145, 620);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(929, 105);
+            panel5.TabIndex = 3;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.employees;
+            pictureBox4.Location = new Point(29, 9);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(90, 90);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 2;
+            pictureBox4.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Cursor = Cursors.Hand;
+            label5.Font = new Font("Lucida Sans", 35F, FontStyle.Bold);
+            label5.ForeColor = Color.FromArgb(94, 21, 4);
+            label5.Location = new Point(180, 24);
+            label5.Name = "label5";
+            label5.Size = new Size(368, 67);
+            label5.TabIndex = 1;
+            label5.Text = "Сотрудники";
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(pictureBox5);
+            panel6.Controls.Add(label6);
+            panel6.Location = new Point(145, 740);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(929, 105);
+            panel6.TabIndex = 4;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.administrator;
+            pictureBox5.Location = new Point(29, 9);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(90, 90);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 2;
+            pictureBox5.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Cursor = Cursors.Hand;
+            label6.Font = new Font("Lucida Sans", 35F, FontStyle.Bold);
+            label6.ForeColor = Color.FromArgb(94, 21, 4);
+            label6.Location = new Point(180, 24);
+            label6.Name = "label6";
+            label6.Size = new Size(472, 67);
+            label6.TabIndex = 1;
+            label6.Text = "Администратор";
+            // 
+            // panel7
+            // 
+            panel7.Controls.Add(pictureBox6);
+            panel7.Controls.Add(label7);
+            panel7.Location = new Point(145, 860);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(929, 105);
+            panel7.TabIndex = 5;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.group;
+            pictureBox6.Location = new Point(29, 9);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(90, 90);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 2;
+            pictureBox6.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Cursor = Cursors.Hand;
+            label7.Font = new Font("Lucida Sans", 35F, FontStyle.Bold);
+            label7.ForeColor = Color.FromArgb(94, 21, 4);
+            label7.Location = new Point(180, 24);
+            label7.Name = "label7";
+            label7.Size = new Size(436, 67);
+            label7.TabIndex = 1;
+            label7.Text = "Зал ожидания";
+            // 
+            // Main_menu_form
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(238, 208, 172);
+            ClientSize = new Size(1918, 1078);
+            ControlBox = false;
+            Controls.Add(panel7);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MaximumSize = new Size(1920, 1080);
+            MinimumSize = new Size(1918, 1078);
+            Name = "Main_menu_form";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = " ";
+            Load += Main_menu_form_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Panel panel3;
+        private PictureBox pictureBox2;
+        private Label label3;
+        private Panel panel4;
+        private PictureBox pictureBox3;
+        private Label label4;
+        private Panel panel5;
+        private PictureBox pictureBox4;
+        private Label label5;
+        private Panel panel6;
+        private PictureBox pictureBox5;
+        private Label label6;
+        private Panel panel7;
+        private PictureBox pictureBox6;
+        private Label label7;
+    }
+}
