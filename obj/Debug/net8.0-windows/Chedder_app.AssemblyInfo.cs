4c4b18c757b4154ddc2726081fe1830e9366c97b
FroStyRo <62169048+VostrikovRoman@users.noreply.github.com>
2024-06-19 16:12:16 +0500

    Какие то новые изменения

--- obj/Debug/net8.0-windows/Chedder_app.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Chedder_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chedder_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec86524cb8b1f684f67b0752e979dbe54811dc7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed4906c36f8d872646a70b08adcffe88b773973")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chedder_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chedder_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
